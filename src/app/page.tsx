f2e96a08b63eb4315b6deacb37d5b55971c79645
Côme Leleu <come.leleu@gmail.com>
2026-01-26 17:21:14 -0800

    [FEATURE] 152115832 Improvement of sections titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="grid sm:grid-cols-[auto_1fr] min-h-screen max-w-7xl mx-auto">
             <Navbar />
 
-            <div className="flex flex-col gap-20 px-8 py-6 sm:py-10">
+            <div className="flex flex-col gap-32 px-8 py-6 sm:py-10">
                 <About />
 
                 <Experiences />
