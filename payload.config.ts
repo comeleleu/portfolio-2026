df1f6586815e12c8689c3e3f509fccd466921372
Côme Leleu <come.leleu@libeo.com>
2026-05-03 00:06:52 -0700

    [FEATURE] 181630831 Database configuration update for Supabase integration

--- payload.config.ts
+++ payload.config.ts
@@ -2,7 +2,6 @@ import sharp from 'sharp'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { buildConfig } from 'payload'
-import { Pool } from 'pg'
 import { Companies } from '@collections/Companies'
 import { Experiences } from '@collections/Experiences'
 import { Links } from '@collections/Links'
@@ -12,13 +11,6 @@ import { Schools } from '@/collections/Schools'
 import { Studies } from '@/collections/Studies'
 import { Tags } from '@collections/Tags'
 import { Sections } from '@globals/Sections'
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
 
 export default buildConfig({
   editor: lexicalEditor(),
@@ -40,7 +32,14 @@ export default buildConfig({
 
 
   secret: process.env.PAYLOAD_SECRET || '',
-  db: postgresAdapter({ pool }),
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL || '',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }),
 
   sharp,
 })
