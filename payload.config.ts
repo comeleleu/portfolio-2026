d6e5bbf89e32ec3b0b3c5d58ec7f6f06d804f78c
Côme Leleu <come.leleu@gmail.com>
2026-05-11 10:41:31 -0700

    [FEATURE] 1816310831 Reorganized imports and removed warnings related to the BLOB_READ_WRITE_TOKEN

--- payload.config.ts
+++ payload.config.ts
@@ -1,7 +1,8 @@
-import sharp from 'sharp'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import { postgresAdapter } from '@payloadcms/db-postgres'
 import { buildConfig } from 'payload'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
+import sharp from 'sharp'
 import { Companies } from '@collections/Companies'
 import { Experiences } from '@collections/Experiences'
 import { Links } from '@collections/Links'
@@ -11,13 +12,6 @@ import { Schools } from '@/collections/Schools'
 import { Studies } from '@/collections/Studies'
 import { Tags } from '@collections/Tags'
 import { Sections } from '@globals/Sections'
-import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
-
-if (!process.env.BLOB_READ_WRITE_TOKEN) {
-  console.warn('⚠️ ATTENTION: BLOB_READ_WRITE_TOKEN est manquant. Le stockage Vercel Blob sera désactivé.');
-} else {
-  console.warn('⚠️ ATTENTION: BLOB_READ_WRITE_TOKEN: ' + process.env.BLOB_READ_WRITE_TOKEN);
-}
 
 export default buildConfig({
   editor: lexicalEditor(),
@@ -53,14 +47,6 @@ export default buildConfig({
       collections: {
         medias: {
           disablePayloadAccessControl: true,
-          generateFileURL: ({ filename, prefix }) => {
-            const token = process.env.BLOB_READ_WRITE_TOKEN || '';
-            const match = token.match(/^vercel_blob_rw_([a-z0-9]+)_[a-zA-Z0-9]+$/i);
-            if (match && match[1]) {
-              return `https://${match[1]}.public.blob.vercel-storage.com/${prefix ? `${prefix}/` : ''}${filename}`;
-            }
-            return `/api/medias/file/${filename}`;
-          },
         },
       },
       token: process.env.BLOB_READ_WRITE_TOKEN,
