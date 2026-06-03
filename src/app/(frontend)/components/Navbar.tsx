df120bf883c63088bf973d6a8bc1b797266284de
Côme Leleu <come.leleu@gmail.com>
2026-06-02 19:52:22 -0700

    [FIX] 186781406 Update Navbar padding on mobile

--- src/app/(frontend)/components/Navbar.tsx
+++ src/app/(frontend)/components/Navbar.tsx
@@ -60,7 +60,7 @@ export const Navbar = ({ sectionParameters }: { sectionParameters: any }) => {
     }, []);
 
     return (
-        <div className="sticky top-0 z-50 flex flex-row md:flex-col items-center w-screen md:w-auto h-auto md:h-screen px-2 sm:px-4 py-2 md:py-8 overflow-hidden">
+        <div className="sticky top-0 z-50 flex flex-row md:flex-col items-center w-screen md:w-auto h-auto md:h-screen px-2 sm:px-4 py-1.5 md:py-8 overflow-hidden">
             <nav
                 className="flex flex-row md:flex-col items-center gap-6 grow w-auto md:w-14 h-14 md:h-auto px-6 md:px-2 py-2 md:py-6 bg-zinc-900/50 md:bg-zinc-900/40 backdrop-blur-lg border border-zinc-800/50 md:border-zinc-700/50 rounded-full"
                 role="navigation"
