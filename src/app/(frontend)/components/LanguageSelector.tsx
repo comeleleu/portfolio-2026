5a28cb0a987644d67de6210126d3e4695a1f903e
Côme Leleu <come.leleu@libeo.com>
2026-06-30 13:13:44 -0700

    [FEATURE] 158170367 Update language selection labels

--- src/app/(frontend)/components/LanguageSelector.tsx
+++ src/app/(frontend)/components/LanguageSelector.tsx
@@ -25,11 +25,11 @@ export const LanguageSelector = () => {
         <a
             href={`/${nextLocale}`}
             className={`
-                fixed z-50 right-2 sm:right-4 bottom-1.5 md:bottom-8 flex items-center justify-center cursor-pointer text-zinc-300 font-semibold hover:text-zinc-100 bg-zinc-900/50 md:bg-zinc-900/40 hover:bg-zinc-800/50 backdrop-blur-lg border border-zinc-800/50 md:border-zinc-700/50 hover:border-zinc-700/50 hover:md:border-zinc-600/50 rounded-full overflow-hidden transition-all duration-500 ease-in-out 
+                fixed z-50 right-2 sm:right-4 bottom-1.5 md:bottom-8 flex items-center justify-center cursor-pointer text-md text-zinc-300 font-semibold hover:text-zinc-100 bg-zinc-900/50 md:bg-zinc-900/40 hover:bg-zinc-800/50 backdrop-blur-lg border border-zinc-800/50 md:border-zinc-700/50 hover:border-zinc-700/50 hover:md:border-zinc-600/50 rounded-full overflow-hidden transition-all duration-500 ease-in-out 
                 ${showLanguageSelector ? 'w-12 h-12 opacity-100 translate-0'
                 : 'w-0 h-0 mr-6 opacity-0 translate-y-24 pointer-events-none'}
             `}
-            aria-label={t('navbar.scrollTop')}
+            aria-label={t('navbar.languageSelector')}
             tabIndex={showLanguageSelector ? 0 : -1}
             aria-hidden={!showLanguageSelector}
         >
